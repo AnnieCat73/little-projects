aa1027f57bb455da9e69798d9e0ba12a640ad21f
Annie <devannie777@gmail.com>
2022-04-30 12:12:01 +0100

    Auto-text-effect project

---
+++ auto-text-effect/script.js
@@ -0,0 +1,18 @@
+const textEl = document.getElementById('text');
+const speedEl = document.getElementById('speed');
+const text = "We Love Programming!";
+let index = 1; //the index starts at 1
+let speed = 1000;//300 or whichever no you prefer
+
+writeText();
+
+function writeText() {
+  textEl.innerText = text.slice(0, index);
+  index++;//go to 2, 3 etc
+  if (index > text.length) {
+    index = 1;//resets to 1 if reaches the end
+  }
+  setTimeout(writeText, speed)
+}
+
+speedEl.addEventListener('input', (e) => speed = 1000 / e.target.value);
